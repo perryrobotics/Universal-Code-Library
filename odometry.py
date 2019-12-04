4a6f2f030245ff174a8fddd5b8486e31c4fcf86d
perryrobotics <47753315+perryrobotics@users.noreply.github.com>
2019-12-04 16:49:03 -0600

    Add files via upload
    Start of odometry file

---
+++ odometry.py
@@ -0,0 +1,18 @@
+LM = 0
+RM = 3
+
+#These need to be changed for your robot!
+TICKS_PER_ROT = 1000
+WHEEL_CIRC_MM =  100
+TICKS_PER_MM = TICKS_PER_ROT/WHEEL_CIRC_MM
+
+def forward_mm(speed, dist):
+    cmpc(LM)
+    cmpc(RM)
+    total_ticks= dist*TICKS_PER_MM
+    mrp(LM, speed, total_ticks)
+    mrp(RM, speed, total_ticks)
+    bmd(RM)
+
+    
+    
