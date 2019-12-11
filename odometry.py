17d2aaea1b883f1ad3cd03daa06531232346c5a7
perryrobotics <47753315+perryrobotics@users.noreply.github.com>
2019-12-11 11:27:46 -0600

    created degrees function. Please check my logic

--- odometry.py
+++ odometry.py
@@ -1,11 +1,19 @@
+#!/usr/bin/python
+import os, sys
+from wallaby import *
+from math import *
 LM = 0
 RM = 3
-
+m = math
+pi = m.pi
 #These need to be changed for your robot!
 TICKS_PER_ROT = 1000
 WHEEL_CIRC_MM =  100
 TICKS_PER_MM = TICKS_PER_ROT/WHEEL_CIRC_MM
-wheel_base = 200
+base = 200
+#base - the distance between the 2 front wheels
+circ_of_base = pi * base
+mm_per_deg = circ_of_base / 360
 
 def forward_mm(speed, dist):
     cmpc(LM)
@@ -43,4 +51,17 @@ def right_mm(speed, dist):
 def left_deg(speed, deg):
     cmpc(LM)
     cmpc(RM)
-    degrees = 
+    mm_to_go = deg * mm_per_deg
+    temp = mm_to_go / 2
+    mrp(LM, -speed, temp)
+    mrp(RM, speed, temp)
+    bmd(RM)
+
+def right_deg(speed, deg):
+    cmpc(LM)
+    cmpc(RM)
+    mm_to_go = deg * mm_per_deg
+    temp = mm_to_go / 2
+    mrp(LM, speed, temp)
+    mrp(RM, -speed, temp) 
+    bmd(RM)
